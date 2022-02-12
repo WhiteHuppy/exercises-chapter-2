e1adc4636c668af370b13c3e09ef3bf150af7d61
Nikhil Ramchandran <nikhil.ramchandran18@imperial.ac.uk>
2022-02-12 10:55:45 +0000

    Updated primes.py.

--- math_utils/primes.py
+++ math_utils/primes.py
@@ -1,7 +1,10 @@
 def isprime(n):
     """Returns True if n is a prime else False"""
+    if n == 1:
+        return False
+    
     max_i = int(n ** 0.5)
     for i in range(2, max_i + 1):
         if n % i == 0:
-            return True
-        return False
+            return False
+    return True
