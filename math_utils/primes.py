56e0bc14e4d410170a64c7a652a4a4c26a6937e5
Nikhil Ramchandran <nikhil.ramchandran18@imperial.ac.uk>
2022-02-12 10:40:31 +0000

    Added a package math_utils.

---
+++ math_utils/primes.py
@@ -0,0 +1,7 @@
+def isprime(n):
+    """Returns True if n is a prime else False"""
+    max_i = int(n ** 0.5)
+    for i in range(2, max_i + 1):
+        if n % i == 0:
+            return True
+        return False
